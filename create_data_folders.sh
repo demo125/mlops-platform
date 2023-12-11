cfb284943b246f34e18c1aece55ec652bdb925e1
Martin Demeter <demeter@mindit.sk>
2023-12-11 23:29:24 +0100

    set up mlflow with minio s3 storage

--- create_data_folders.sh
+++ create_data_folders.sh
@@ -4,6 +4,7 @@ mkdir volume
 
 mkdir $VOLUME_PATH_AGENT_0/minio -p
 mkdir $VOLUME_PATH_AGENT_0/mlflow/postgres -p
+mkdir $VOLUME_PATH_AGENT_0/mlflow/minio -p
 mkdir $VOLUME_PATH_AGENT_0/docker-registry -p
 
 chgrp 1001 volume  -R
