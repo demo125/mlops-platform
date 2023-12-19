2f536cfa90c81e33b88cd7e33f2045fcd292a582
Martin Demeter <demeter@mindit.sk>
2023-12-19 12:20:47 +0100

    jenkins helm chart

--- create_data_folders.sh
+++ create_data_folders.sh
@@ -3,6 +3,7 @@ source .env
 mkdir volume
 
 mkdir $VOLUME_PATH_AGENT_0/minio -p
+mkdir $VOLUME_PATH_AGENT_0/jenkins -p
 mkdir $VOLUME_PATH_AGENT_0/mlflow/postgres -p
 mkdir $VOLUME_PATH_AGENT_0/mlflow/minio -p
 mkdir $VOLUME_PATH_AGENT_0/docker-registry -p
