71bb12b7ddadf76f4b1be7c17bfd98f2937470f8
Martin Demeter <demeter@mindit.sk>
2024-01-19 16:03:31 +0100

    added cluster issuer

--- cert-manager/down.sh
+++ cert-manager/down.sh
@@ -1,1 +1,3 @@
 kubectl delete -f cert-manager.yaml
+kubectl delete -f certificate.yaml
+kubectl delete -f cluster-issuer.yaml
--- cert-manager/up.sh
+++ cert-manager/up.sh
@@ -1,1 +1,3 @@
 kubectl apply -f cert-manager.yaml
+kubectl apply -f certificate.yaml
+kubectl apply -f cluster-issuer.yaml
