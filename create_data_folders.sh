9cc75911d79d6a3d264669201f5784c7b6ea2097
Martin Demeter <demeter@mindit.sk>
2024-02-13 22:18:38 +0100

    fix

--- create_data_folders.sh
+++ create_data_folders.sh
@@ -13,6 +13,7 @@ mkdir $VOLUME_PATH_AGENT_0/prometheus-stack/grafana -p
 mkdir $VOLUME_PATH_AGENT_0/mlflow/postgres -p
 mkdir $VOLUME_PATH_AGENT_0/mlflow/minio -p
 mkdir $VOLUME_PATH_AGENT_0/docker-registry -p
+mkdir $VOLUME_PATH_AGENT_0/prod/example-project -p
 
 # chgrp 1001 volume  -R
 ls -l volume
